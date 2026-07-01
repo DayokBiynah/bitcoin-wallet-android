2275ca49356ed6344c702aa82f5939a38be5c956
Biynah Ayobami Dayok <biynahdayok1@gmail.com>
2026-07-01 12:51:13 -0500

    Add wallet fragments and resource cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,13 +18,12 @@ android {
 
     buildTypes {
         getByName("debug") {
-            debuggable(true)
+            isDebuggable = true
         }
         getByName("release") {
-            debuggable(false)
+            isDebuggable = false
             // minifyEnabled(false)
             // shrinkResources(true)
-            proguardFiles = mutableListOf(getDefaultProguardFile("proguard-android-optimize.txt"), file("proguard-rules.pro"))
         }
     }
 
@@ -39,12 +38,15 @@ android {
 }
 
 dependencies {
+    implementation("androidx.activity:activity-ktx:1.3.1")
     // basic android dependencies
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
     implementation ("androidx.core:core-ktx:1.5.0")
     implementation ("androidx.appcompat:appcompat:1.3.0")
     implementation ("com.google.android.material:material:1.3.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     // tests
     testImplementation ("junit:junit:4.+")
--- app/src/main/java/org/summerofbitcoin/wallet/MainActivity.kt
+++ app/src/main/java/org/summerofbitcoin/wallet/MainActivity.kt
@@ -2,10 +2,12 @@ package org.summerofbitcoin.wallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.content.Intent
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, WalletChoiceActivity::class.java))
+        finish()
     }
 }
